3e604cd984b71115a3cde6cce52ad50896220fe3
sikoob <simon.koob@gmx.net>
2022-02-08 20:35:34 +0100

    working general setup

--- index.js
+++ index.js
@@ -3,22 +3,69 @@
 jQuery(document).ready(function($) {
     $(".contact__info").hide();
     $(".news__content").hide();
+    $(".image__architecture").hide();
+    $(".image__design").hide();
+    $(".image__light").hide();
+    $(".image__gastro").hide();
+
+    $(".whole_page").hover(
+        function() {
+            $(".contact__info").hide();
+            $(".news__content").hide();
+            $(".image__architecture").hide();
+            $(".image__design").hide();
+            $(".image__light").hide();
+            $(".image__gastro").hide();
+        }
+    );
 
     $(".content__contact").hover(
         function() {
+            $(".news__content").hide();
             $(".contact__info").show();
-        },
-        function() {
-            $(".contact__info").hide();
         }
     );
 
     $(".content__news").hover(
         function() {
+            $(".contact__info").hide();
             $(".news__content").show();
-        },
+        }
+    );
+
+    $(".content__architecutre").hover(
         function() {
-            $(".news__content").hide();
+            $(".image__design").hide();
+            $(".image__light").hide();
+            $(".image__gastro").hide();
+            $(".image__architecture").show();
+        }
+    );
+
+    $(".content__design").hover(
+        function() {
+            $(".image__architecture").hide();
+            $(".image__light").hide();
+            $(".image__gastro").hide();
+            $(".image__design").show();
+        }
+    );
+
+    $(".content__light").hover(
+        function() {
+            $(".image__architecture").hide();
+            $(".image__design").hide();
+            $(".image__gastro").hide();
+            $(".image__light").show();
+        }
+    );
+
+    $(".content__gastro").hover(
+        function() {
+            $(".image__architecture").hide();
+            $(".image__design").hide();
+            $(".image__light").hide();
+            $(".image__gastro").show();
         }
     );
 });
