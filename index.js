ae81c71109eb7600e1b8814da71d36044534cde8
sikoob <simon.koob@gmx.net>
2022-02-08 21:04:04 +0100

    fixing hovering problem with images

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 jQuery(document).ready(function($) {
+    
     $(".contact__info").hide();
     $(".news__content").hide();
     $(".image__architecture").hide();
@@ -8,64 +9,56 @@ jQuery(document).ready(function($) {
     $(".image__light").hide();
     $(".image__gastro").hide();
 
-    $(".whole_page").hover(
-        function() {
-            $(".contact__info").hide();
-            $(".news__content").hide();
-            $(".image__architecture").hide();
-            $(".image__design").hide();
-            $(".image__light").hide();
-            $(".image__gastro").hide();
-        }
-    );
-
     $(".content__contact").hover(
         function() {
-            $(".news__content").hide();
             $(".contact__info").show();
+            $(".news__content").hide();
         }
     );
 
     $(".content__news").hover(
         function() {
-            $(".contact__info").hide();
             $(".news__content").show();
+            $(".contact__info").hide();
         }
     );
 
-    $(".content__architecutre").hover(
+    $(".whole__content.content__architecutre").hover(
         function() {
-            $(".image__design").hide();
-            $(".image__light").hide();
-            $(".image__gastro").hide();
             $(".image__architecture").show();
+        },function() {
+            $(".image__architecture").hide();
         }
     );
 
-    $(".content__design").hover(
+    $(".whole__content.content__design").hover(
         function() {
-            $(".image__architecture").hide();
-            $(".image__light").hide();
-            $(".image__gastro").hide();
             $(".image__design").show();
+        },function() {
+            $(".image__design").hide();
         }
     );
 
-    $(".content__light").hover(
+    $(".whole__content.content__light").hover(
         function() {
-            $(".image__architecture").hide();
-            $(".image__design").hide();
-            $(".image__gastro").hide();
             $(".image__light").show();
+        },function() {
+            $(".image__light").hide();
         }
     );
 
-    $(".content__gastro").hover(
+    $(".whole__content.content__gastro").hover(
         function() {
-            $(".image__architecture").hide();
-            $(".image__design").hide();
-            $(".image__light").hide();
             $(".image__gastro").show();
+        },function() {
+            $(".image__gastro").hide();
+        }
+    );
+
+    $(".whole_page").hover(
+        function() {
+            $(".contact__info").hide();
+            $(".news__content").hide();
         }
     );
 });
