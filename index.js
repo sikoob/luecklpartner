f49f879a44d9699fe81981c171aadd9a8e106c60
sikoob <simon.koob@gmx.net>
2022-02-09 17:36:22 +0100

    working on responsive setup

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ jQuery(document).ready(function($) {
         }
     );
 
+    $(".contact__info").hover(
+        function() {
+            $(".contact__info").show();
+        }
+    );
+
     $(".content__contact").click(
         function() {
             $(".contact__info").toggle();
@@ -35,7 +41,7 @@ jQuery(document).ready(function($) {
         }
     );
 
-    $(".whole__content.content__architecutre").hover(
+    $(".content__architecutre").hover(
         function() {
             $(".image__architecture").show();
         },function() {
@@ -43,13 +49,13 @@ jQuery(document).ready(function($) {
         }
     );
 
-    $(".whole__content.content__architecutre").click(
+    $(".content__architecutre").click(
         function() {
             $(".image__architecture").toggle();
         }
     );
 
-    $(".whole__content.content__design").hover(
+    $(".content__design").hover(
         function() {
             $(".image__design").show();
         },function() {
@@ -57,13 +63,13 @@ jQuery(document).ready(function($) {
         }
     );
 
-    $(".whole__content.content__design").click(
+    $(".content__design").click(
         function() {
             $(".image__design").toggle();
         }
     );
 
-    $(".whole__content.content__light").hover(
+    $(".content__light").hover(
         function() {
             $(".image__light").show();
         },function() {
@@ -71,14 +77,14 @@ jQuery(document).ready(function($) {
         }
     );
 
-    $(".whole__content.content__light").click(
+    $(".content__light").click(
         function() {
             $(".image__light").toggle();
         }
     );
 
 
-    $(".whole__content.content__gastro").hover(
+    $(".content__gastro").hover(
         function() {
             $(".image__gastro").show();
         },function() {
@@ -86,7 +92,7 @@ jQuery(document).ready(function($) {
         }
     );
 
-    $(".whole__content.content__gastro").click(
+    $(".content__gastro").click(
         function() {
             $(".image__gastro").toggle();
         }
